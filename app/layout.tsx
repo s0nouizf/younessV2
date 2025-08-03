665f572bd1f21b9e643bb28b7606b003dd6d27e0
you <you@gmail.com>
2025-08-03 17:58:38 +0200

    baba

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'cvdigital',
+  description: 'Created with cvdigital',
+  generator: 'cvdigital',
 }
 
 export default function RootLayout({
